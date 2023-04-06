96703db3e46f10f9445a98e11555de2b654f7690
Jácson Silva <71229138+1ELShiroe@users.noreply.github.com>
2023-04-06 19:30:27 -0300

    foreach

---
+++ foreach/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+
+string[] ids = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string id in ids)
+{
+    if (id.StartsWith("B")) Console.WriteLine($"The name starts with 'B'! \nValue: {id}");
+}
