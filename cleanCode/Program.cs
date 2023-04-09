aea1490962143d3cfdc59169b7617fa53652ed1b
Jácson Silva <71229138+1ELShiroe@users.noreply.github.com>
2023-04-09 11:30:14 -0300

    Update Program.cs

--- cleanCode/Program.cs
+++ cleanCode/Program.cs
@@ -1,18 +1,13 @@
-// /*
-//    This code reverses a message, counts the number of times 
-//    a particular character appears, then prints the results
-//    to the console window.
-//  */
-// Console.Clear();
-// char[] message = "The quick brown fox jumps over the lazy dog.".ToCharArray();
-// Array.Reverse(message);
+Console.Clear();
+char[] message = "The quick brown fox jumps over the lazy dog.".ToCharArray();
+Array.Reverse(message);
 
-// int letterCount = 0;
+int letterCount = 0;
 
-// foreach (char letter in message)
-// {
-//     if (letter == 'o') letterCount++;
-// }
+foreach (char letter in message)
+{
+    if (letter == 'o') letterCount++;
+}
 
-// Console.WriteLine(new String(message));
-// Console.WriteLine($"'o' appears {letterCount} times.");
+Console.WriteLine(new String(message));
+Console.WriteLine($"'o' appears {letterCount} times.");
